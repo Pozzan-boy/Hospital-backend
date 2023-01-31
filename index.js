8e514e9d0e11c5420029bfabd4dc0cd3ac1cdc7c
Stanislaw <stasmohoid228337@gmil.com>
2023-01-31 18:02:17 +0200

    changed port to 3001

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, (err) => {
+app.listen(3001, (err) => {
     if (err) {
         return console.log(err);        
     }
