6ecc812666f047c2608f32ba62d86991aa6d7fee
Stanislaw <stasmohoid228337@gmil.com>
2023-02-07 14:50:20 +0200

    added login with token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,24 @@ import UserModel from '../models/User.js';
 
 export const login = async (req, res) => {
     try {
+
+        if(req.headers.authorization !== undefined) {
+            const codedToken = req.headers.authorization.replace(/Bearer\s?/, '');
+            try {
+                const decoded = jwt.verify(codedToken, 'pass123');
+                const user = await UserModel.findById(decoded._id);
+                const {...userData} = user._doc;
+                return res.json({
+                    ...userData,
+                    token: codedToken
+                });
+            } catch(e) {
+                return res.status(404).json({
+                    message: 'Login fail!'
+                });
+            }
+        }
+
         const user = await UserModel.findOne({login: req.body.login, role: req.body.role});
         if (!user) {
             return res.status(404).json({
